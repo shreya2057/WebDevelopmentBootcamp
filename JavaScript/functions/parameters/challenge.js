35d5919358e21383f75a8099308fdd6d46bcaf04
Shreya Shrestha <shreya2057@gmail.com>
2021-11-23 21:28:44 +0545

    9. Functions

---
+++ JavaScript/functions/parameters/challenge.js
@@ -0,0 +1,19 @@
+var yourAge = prompt("Enter your Age");
+getLifeInWeeks(yourAge);
+
+function getLifeInWeeks(yourAge){
+
+    var aliveAge = 90;
+    var remainingAge = 90 - yourAge;
+
+    var days = 365;
+    var weeks = 52;
+    var months = 12;
+
+    var daysRemaining = remainingAge * days;
+    var weeksRemaining = remainingAge * weeks;
+    var monthsRemaining = remainingAge * months;
+
+    console.log("You have," + " " + daysRemaining + " " + "days," + " " + weeksRemaining + " " + "weeks and" + " " + monthsRemaining + " " + "months left");
+
+}
