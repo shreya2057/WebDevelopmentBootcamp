9c1f66c003f2f7cd1ea6fce92dd22eb4b7d980c4
Shreya Shrestha <shreya2057@gmail.com>
2021-11-23 21:27:29 +0545

    7. Datatypes

---
+++ JavaScript/datatypes/data.js
@@ -0,0 +1,11 @@
+console.log(2+3);
+console.log(typeof(23));
+
+var myName = "Shreya";
+
+alert(myName);
+
+var yourName = prompt("Enter your name");
+alert(yourName);
+
+alert("I am " + myName + "Welcome to my website " + yourName + "!");
