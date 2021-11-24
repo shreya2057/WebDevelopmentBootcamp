e7d9760d44fb6d81e02241282ac2b82357d7e09c
Shreya Shrestha <shreya2057@gmail.com>
2021-11-24 15:56:40 +0545

    Random Number generator

---
+++ random_number/random.js
@@ -0,0 +1,8 @@
+
+var name1 = prompt("Enter the name of person1: ");
+var name2 =  prompt("Enter the name of person2: ");
+
+var n = Math.random()*100;
+n = Math.floor(n)+1;
+
+alert("The percentage is " + n + "%");
