987e28360eabd456f9a5f7ed95c62558535811cb
Shreya Shrestha <shreya2057@gmail.com>
2021-11-29 09:12:40 +0545

    Comaparators of javascript

---
+++ comparators/comp.js
@@ -0,0 +1,15 @@
+
+var a = 1;
+var b = "1";
+
+if(a===b){                  //checks datatype along with the values
+    alert("a===b is true"); 
+}else{
+    alert("a===b is false");
+}
+
+if(a==b){                   //simply checks for value only
+    alert("a==b is true");
+}else{
+    alert("a==b is false");
+}
